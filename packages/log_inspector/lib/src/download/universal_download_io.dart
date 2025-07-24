80c38b04318c0403280f3d05987e5f051b79ce20
Aleksander Hrubalski <16018532+hrubalski@users.noreply.github.com>
2025-07-24 12:19:20 +0200

    fix: update Flutter SDK constraint to allow for all versions >=3.0.0 and remove unused import in universal_download_io.dart

--- packages/log_inspector/lib/src/download/universal_download_io.dart
+++ packages/log_inspector/lib/src/download/universal_download_io.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'dart:ui';
-import 'package:flutter/foundation.dart';
 import 'package:log_inspector/src/utils/extensions/date_time_extension.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
